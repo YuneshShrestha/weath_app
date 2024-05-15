104381338a7dd32ffafa4dc848efd11637fb887e
Yunesh Shrestha <yuneshshrestha24@gmail.com>
2024-05-15 09:33:29 +0545

    push unwanted imports

--- lib/view/widgets/temperature_view_widget.dart
+++ lib/view/widgets/temperature_view_widget.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_weather_app/controller/check_if_skip.dart';
 import 'package:flutter_weather_app/view/pages/help_screen.dart';
-import 'package:get/get.dart';
 
 class TemperatureViewWidget extends StatelessWidget {
   const TemperatureViewWidget(
